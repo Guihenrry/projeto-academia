69650cda9c4edaa6f4d5be7d30bda598ed8b8555
guihenrry <guilhermehenry60@gmail.com>
2018-08-13 17:08:54 -0300

    adicionado javascript para abrir o conteudo dos treinos

---
+++ javascript/treino.js
@@ -0,0 +1,20 @@
+function openTreino(evt, nameTreino){
+	var tabcontent, tablinks;
+
+	/* Deixar todos os conteudo com display none*/
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (var i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	/* Deixar todos os botões sem a class active*/
+	tablinks = document.getElementsByClassName("tablinks");
+	for (var i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	/* show no conteudo e colocar active no botão*/
+	document.getElementById(nameTreino).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
